eb793a57642835c824c79a59a8d5dcf3643a64eb
NaufalRafif <77334971+ThePushi@users.noreply.github.com>
2021-03-14 19:58:47 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('ready', () => {
         client.user.setStatus('idle')
         client.user.setPresence({
             game: {
-            name: "Players: " online,
+            name: "Players: " + response['online'],
             type: "PLAYING",
             url: "https://www.unityofindonesia.com/"
         }
